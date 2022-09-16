2d50f64fb7fc9f95a76830b5d11f2054edb9b8fa
Hugo Lyppens <hugo@lyppens.net>
2022-09-16 10:36:09 -0400

    Update client-side libraries Update NuGet ServiceModel from 4.9.0 to 4.10.0

--- Connected Services/GD3/Reference.cs
+++ Connected Services/GD3/Reference.cs
@@ -2994,7 +2994,7 @@ namespace GD3
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginOpen(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndOpen));
         }
         
-        public virtual System.Threading.Tasks.Task CloseAsync()
+        public new virtual System.Threading.Tasks.Task CloseAsync()
         {
             return System.Threading.Tasks.Task.Factory.FromAsync(((System.ServiceModel.ICommunicationObject)(this)).BeginClose(null, null), new System.Action<System.IAsyncResult>(((System.ServiceModel.ICommunicationObject)(this)).EndClose));
         }
